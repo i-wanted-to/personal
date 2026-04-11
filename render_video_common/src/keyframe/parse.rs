5519831d2f511c69e110d36cda47f1f355053865
i-wanted-to <personal_github@iwantedto.com>
2026-04-11 12:50:25 +0000

    expose parsing traits

--- render_video_common/src/keyframe/parse.rs
+++ render_video_common/src/keyframe/parse.rs
@@ -2,9 +2,9 @@ mod __ { /*
 
 contents omitted by gitstub
 
-246 lines
-9265 bytes
+312 lines
+10857 bytes
 
-79e9ccdb7b6662aae5c6d6fbb27c941aa4d3372c
+7637f77276e1f7320fba2ead491b41d43891c279
 
 */ }
