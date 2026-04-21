3c2d1eb4b4a3e9966bd8db6990e1a75bb06dd350
i-wanted-to <personal_github@iwantedto.com>
2026-04-21 15:30:41 +0000

    cascade style

--- atlas_generator/src/source/load/image/build/interpret.rs
+++ atlas_generator/src/source/load/image/build/interpret.rs
@@ -2,9 +2,9 @@ mod __ { /*
 
 contents omitted by gitstub
 
-395 lines
-14786 bytes
+420 lines
+15745 bytes
 
-b7bb50772fb2fac4c321f36f61207fc0276b6dba
+dd27a2076290c7949ed3fa241fc5c0b2f0c5daf3
 
 */ }
--- atlas_generator/src/vector/scene.rs
+++ atlas_generator/src/vector/scene.rs
@@ -3,8 +3,8 @@ mod __ { /*
 contents omitted by gitstub
 
 91 lines
-2033 bytes
+2039 bytes
 
-58fd72557ae492ca92d77c1087794224595d8e7f
+5be81bd520efc479a18a01503a80ef212a2496d0
 
 */ }
