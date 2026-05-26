47980e26fc2635ea137da768e96ce609a701134b
i-wanted-to <personal_github@iwantedto.com>
2026-05-26 18:25:42 +0000

    reject labeled files

--- archive/src/main.rs
+++ archive/src/main.rs
@@ -2,9 +2,9 @@ mod __ { /*
 
 contents omitted by gitstub
 
-283 lines
-10060 bytes
+291 lines
+10325 bytes
 
-1fd60f7b2206c9bbd7ba49ce178f58acade07f81
+543e670ca0fbfb6498ee0adaa6537926183a82bf
 
 */ }
