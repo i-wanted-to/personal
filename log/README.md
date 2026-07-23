204e9e436bb4a4fdcf84dba6e9ee1f0a4580f466
i-wanted-to <personal_github@iwantedto.com>
2026-07-23 12:28:41 +0000

    add log

---
+++ log/README.md
@@ -0,0 +1,3 @@
+# log
+
+Simple logging to file and/or stdout.
---
+++ log/src/lib.rs
@@ -0,0 +1,10 @@
+mod __ { /*
+
+contents omitted by gitstub
+
+80 lines
+2357 bytes
+
+a344535a5b764023654e9f97ff918d3092561584
+
+*/ }
