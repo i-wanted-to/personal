af6baa858a6cde291ae025e68c361a3ecc50232b
i-wanted-to <personal_github@iwantedto.com>
2026-08-11 11:52:04 +0000

    exit early on tls termination

--- proxy/src/client.rs
+++ proxy/src/client.rs
@@ -2,9 +2,9 @@ mod __ { /*
 
 contents omitted by gitstub
 
-98 lines
-3775 bytes
+101 lines
+3849 bytes
 
-e0bb236bdcc7aafb8f15deaecf3e53d0d21cf516
+7cb19d4cd7f1b5b634f57d8141e12b0c4b63e096
 
 */ }
