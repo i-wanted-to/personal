861ad0c64056b2682bc48668d71f9abb39fa0644
i-wanted-to <personal_github@iwantedto.com>
2026-08-16 15:03:00 +0000

    reduce clip transmission

--- relay/src/server/content.rs
+++ relay/src/server/content.rs
@@ -2,9 +2,9 @@ mod __ { /*
 
 contents omitted by gitstub
 
-181 lines
-6290 bytes
+184 lines
+6358 bytes
 
-6b4a72ace1aa9a0d5a33488737cf88296a61a9df
+2f33a5aa94e55c19439c69796945b0813ee8f62d
 
 */ }
--- relay/src/server/mod.rs
+++ relay/src/server/mod.rs
@@ -5,6 +5,6 @@ contents omitted by gitstub
 435 lines
 16508 bytes
 
-a31ea85c5d1ffb56f20e11438304458e80b85fe8
+0a10dd4a83b0023ac9d56a7a4863e7f83c72addc
 
 */ }
